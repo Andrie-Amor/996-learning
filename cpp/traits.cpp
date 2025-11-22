b54047d57577125fc2f03f3285f32c51e5118e76
Andre Amor <ramorSTEM2022@gmail.com>
2025-11-21 23:52:28 -0800

    template specialization

---
+++ cpp/traits.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+// Primary template
+template <typename T>
+struct Printer {
+  static void print(const T& value) {
+    std::cout << "generic: " << value << std::endl;
+  }
+};
+
+// Full specialization
+// Must appear after the primary is declared; must match exact arguments
+template <>
+struct Printer<std::string> {
+  static void print(const std::string& value) {
+    std::cout << "std::string specialized: " << value << std::endl;
+  }
+};
+
+// Partial specialization
+// Customize subset of args or pattern
+// NOTE: For primary specialization, can't define like struct IsPointer<T> or
+// IsPointer<>.
+// NOTE: Only for class/variable templates, NOT function templates
+template <typename T>
+struct IsPointer {
+  static constexpr bool value = false;
+};
+
+// Partial specialization
+template <typename T>
+struct IsPointer<T*> {  // Only takes pointer types
+  static constexpr bool value = true;
+};
+
+// Example 2:
+template <typename Key, typename Value>
+class KeyValuePair {};
+
+template <>
+class KeyValuePair<int, std::string> {};
+
+template <typename Key>
+class KeyStringPair : public KeyValuePair<Key, std::string> {};
+
+template <typename Value>
+class IntegerValuePair : public KeyValuePair<int, Value> {};
